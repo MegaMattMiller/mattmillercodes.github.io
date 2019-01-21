152efa1cbdd9c5e84175cc03433553c0301a5e08
Matt Miller <matt@megamattmiller.com>
2019-01-21 11:58:10 -0600

    docs: whitespace changes

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,10 +4,10 @@
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                <router-link class="nav-link" to="/">Home</router-link>
+                    <router-link class="nav-link" to="/">Home</router-link>
                 </li>
                 <li class="nav-item">
-                <router-link class="nav-link" to="/about">About</router-link>
+                    <router-link class="nav-link" to="/about">About</router-link>
                 </li>
             </ul>
         </div>
