68d18b0f86cc2e97f97272363ddf04e37f90543b
Matt Miller <matt@megamattmiller.com>
2019-02-19 14:27:21 -0600

    feat: added back in site icons

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,18 @@
 <template>
   <b-navbar toggleable="lg" type="dark" variant="dark">
     <b-navbar-brand to="/">Matt Miller | Programmer</b-navbar-brand>
-    <b-navbar-toggle target="nav_collapse" />
+    <b-navbar-toggle target="nav_collapse"/>
     <b-collapse is-nav id="nav_collapse">
       <b-navbar-nav>
         <b-nav-item to="/">Home</b-nav-item>
+      </b-navbar-nav>
+      <b-navbar-nav class="ml-auto">
+        <a class="navbar-icon" href="https://github.com/MegaMattMiller/" target="_blank">
+          <font-awesome-icon :icon="['fab', 'github-square']"/>
+        </a>
+        <a class="navbar-icon" href="https://dev.to/megamattmiller" target="_blank">
+          <font-awesome-icon :icon="['fab', 'dev']"/>
+        </a>
       </b-navbar-nav>
     </b-collapse>
   </b-navbar>
@@ -18,8 +26,8 @@ export default {
 
 <style scoped>
 .navbar-icon {
-    color: white;
-    font-size: 30px;
-    padding-right: 10px
+  color: white;
+  font-size: 30px;
+  padding-right: 10px;
 }
 </style>
